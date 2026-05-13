fa6610e460347c5ba35d7e5723b1943b1c5369c3
berlin1311 <berlincamacho4@gmail.com>
2026-05-12 22:55:57 -0500

    agregando componentes

---
+++ app/components/ui/button.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+export default function home() {
+  return
+  <div>
+    <h1>proyecto</h1>
+    <Button variant="outline">Button</Button>
+  </div>
+}
